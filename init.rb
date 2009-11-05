746043bcad45875c9164fc3e087d502700190ec5
Tony Wieczorek <tonyjw@gmail.com>
2009-11-04 21:45:57 -0500

    getting rid of redis

--- init.rb
+++ init.rb
@@ -9,7 +9,6 @@ rescue LoadError
 end
 
 require "monk/glue"
-require "ohm"
 require "haml"
 require "sass"
 
@@ -17,9 +16,6 @@ class Main < Monk::Glue
   set :app_file, __FILE__
   use Rack::Session::Cookie
 end
-
-# Connect to redis database.
-Ohm.connect(settings(:redis))
 
 # Load all application files.
 Dir[root_path("app/**/*.rb")].each do |file|
